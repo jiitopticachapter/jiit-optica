3659a18a7bf66a602d7e76a4c57d0273466fe223
kshitij <kshitijgupta070704@gmail.com>
2023-10-29 13:42:40 +0530

    Display card feature in react js has been corrected

---
+++ task3/kshitij/src/Values.jsx
@@ -0,0 +1,23 @@
+import './index.css'
+
+
+function App() {
+
+    return (
+        <div>
+            <div>
+                id: "1", <br />
+                name: "kshitij", <br />
+                email: "k@gmail.com" <br />
+            </div>
+            <br /><br />
+            <div>
+                id: "2"   <br />
+                name: "siddhant",  <br />
+                email: "sid@gmail.com" <br />
+            </div>
+        </div>
+    )
+}
+
+export default App
